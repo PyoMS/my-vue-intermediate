3c2a7f3d8d8db1cb2af88e3a506bd61116526363
PMS <ms147850@gmail.com>
2023-02-01 23:15:29 +0900

    fix: set TodoInput Component / 로컬스토리지 설정

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    input
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoInput"
+  name: "TodoInput",
+  data: function () {
+    return {
+      newTodoItem: "",
+
+    }
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem); // 로컬스토리지 - MDN 참고
+      this.newTodoItem = '';
+    },
+  }
 }
 </script>
 
