9b84a102072b53be235d15513e5c8ad2ca5bbc57
PMS <ms147850@gmail.com>
2023-02-02 20:56:22 +0900

    fix: index.html 수정 / set input box style

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button v-on:click="addTodo">add</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+<!--    <button v-on:click="addTodo">add</button>-->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -17,12 +20,39 @@ export default {
   methods: {
     addTodo() {
       localStorage.setItem(this.newTodoItem, this.newTodoItem); // 로컬스토리지 - MDN 참고
+      this.clearInput();
+    },
+    clearInput() {
       this.newTodoItem = '';
     },
+
   }
 }
 </script>
 
 <style scoped>
-
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
