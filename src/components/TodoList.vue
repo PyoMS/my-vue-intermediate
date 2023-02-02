cc66848a2f05b53cc42817402c9ca689213fc68a
PMS <ms147850@gmail.com>
2023-02-02 21:27:04 +0900

    fix: set TodoList 구성

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,74 @@
 <template>
   <div>
-    list
+    <ul>
+      <li class="shadow" v-for="item in todoItems" v-bind:key="item">
+        {{item}}
+        <span class="removeBtn" v-on:click="removeTodo">
+          <i class="fa-solid fa-trash"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  data() {
+    return {
+      todoItems: [],
+
+    };
+  },
+  created() {
+    // console.log('created');
+    if( localStorage.length>0){
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo() {
+    }
+  },
+
+
 }
 </script>
 
-<style scoped>
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 
 </style>
