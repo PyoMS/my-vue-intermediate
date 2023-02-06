14d21ac6ee5128a6caf7fcad03892014c0877e3a
PMS <ms147850@gmail.com>
2023-02-06 21:31:00 +0900

    feat: vuex install and setting

---
+++ src/store/store.js
@@ -0,0 +1,32 @@
+// import { createApp } from 'vue'
+// import { createStore } from 'vuex'
+//
+// // Create a new store instance.
+// const store = createStore({
+//     state () {
+//         return {
+//             count: 0
+//         }
+//     },
+//     mutations: {
+//         increment (state) {
+//             state.count++
+//         }
+//     }
+// })
+//
+// const app = createApp({ /* your root component */ })
+//
+// // Install the store instance as a plugin
+// app.use(store)
+
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+
+
+});
