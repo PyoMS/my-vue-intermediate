9c6b9ec78fdf7ace9355cf6a3cb8497ec6f0d0f4
PMS <ms147850@gmail.com>
2023-02-08 22:03:19 +0900

    feat: 스토어 모듈화

---
+++ src/store/modules/todoApp.js
@@ -0,0 +1,54 @@
+const storage = {
+    fetch() {
+        const arr = [];
+        console.log('created');
+        if( localStorage.length>0){
+            for (let i = 0; i < localStorage.length; i++) {
+                let parseItem = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                arr.push(parseItem);
+            }
+        }
+        return arr;
+    },
+}
+
+const state = {
+    todoItems: storage.fetch(),
+};
+const getters = {
+    storedTodoItems(state) {
+        return state.todoItems;
+    }
+};
+const mutations = {
+    addOneItem(state, todoItem) {
+        const obj = {completed: false, item: todoItem,};
+        localStorage.setItem(todoItem, JSON.stringify(obj)); // 로컬스토리지 - MDN 참고
+        state.todoItems.push(obj); // 첫번째 인자인 state로 접근해야한다.
+    },
+    removeOneItem(state, payload) {
+        console.log(state.todoItems[payload.index])
+        localStorage.removeItem(payload.todoItem.item); // key, value가 동일할 때만
+        state.todoItems.splice(payload.index, 1); // slice와 혼동 주의
+    },
+    toggleOneItem(state, payload) {
+        console.log(state.todoItems[payload.index]);
+        state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed
+
+        // localStorage 는 삭제/추가로 업데이트해야 함.
+        localStorage.removeItem(payload.todoItem.item);
+        localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+    },
+    clearAllItems(state) {
+        localStorage.clear();
+        state.todoItems = [];
+    },
+
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,10 @@
 import { createStore } from 'vuex'
-import * as getters from './getters'
-import * as mutations from './mutations'
-
-const storage = {
-    fetch() {
-        const arr = [];
-        console.log('created');
-        if( localStorage.length>0){
-            for (let i = 0; i < localStorage.length; i++) {
-                let parseItem = JSON.parse(localStorage.getItem(localStorage.key(i)));
-                arr.push(parseItem);
-            }
-        }
-        return arr;
-    },
-}
+import todoApp from './modules/todoApp'
 
 const store = createStore({
-    state () {
-        return {
-            todoItems: storage.fetch(),
-        }
-    },
-    getters,
-    mutations
+    modules: {
+        todoApp
+    }
 
 })
 
